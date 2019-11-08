6ca667310e0d59befcd63d23f11e97001a97626e
Hester Knight <hesterknight@Hesters-MacBook-Pro.local>
2019-11-08 09:09:46 +0000

    get request api data, used use effect, created new character component

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import Character from './components/Characters/Character'
+// import styled from 'styled-components';
 
 const App = () => {
+const [people, setPeople] = useState([])
+
+  useEffect(() => {
+
+    axios.get('https://swapi.co/api/people').then(response => {
+      setPeople(response.data.results);
+  
+
+  });
+},[]);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -13,6 +25,7 @@ const App = () => {
     <div className="App">
       <h1 className="Header">React Wars</h1>
     </div>
+  //  {people.map(character => Character name ={character.name})}
   );
 }
 

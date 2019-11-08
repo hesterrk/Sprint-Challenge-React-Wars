01bbcf4143b90af0e95f7f8935a52f2c7ed77277
Hester Knight <hesterknight@Hesters-MacBook-Pro.local>
2019-11-08 09:31:58 +0000

    added a style

--- starwars/src/components/Characters/Character.js
+++ starwars/src/components/Characters/Character.js
@@ -3,11 +3,13 @@ import { styledCharacter } from './styledCharacter';
 
 function Character(props) {
     return (
+        <styledCharacter>
         <div>
             <p>{props.name}</p>
             <p>{props.birth_year}</p>
 
         </div>
+        </styledCharacter>
     );
 }
 

739783817954b5df0554257b8663813c7797fed5
Hester Knight <hesterknight@Hesters-MacBook-Pro.local>
2019-11-08 09:10:22 +0000

    created character component and given it props, imported and exported it

--- starwars/src/components/Characters/Character.js
+++ starwars/src/components/Characters/Character.js
@@ -1,1 +1,17 @@
+import React from 'react';
 
+function Character(props) {
+    return (
+        <div>
+            <p>{props.name}</p>
+            <p>{props.birth_year}</p>
+
+        </div>
+    )
+}
+
+
+
+
+
+export default Character
